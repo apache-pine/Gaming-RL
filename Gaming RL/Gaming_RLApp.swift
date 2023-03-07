c7f56ee07d0ab1465d7a6e115aacd249b59f5536
Kylar Sorensen <kylar.sorensen@gmail.com>
2023-03-07 09:22:03 -0800

    Initial Commit

---
+++ Gaming RL/Gaming_RLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gaming_RLApp.swift
+//  Gaming RL
+//
+//  Created by Laura Ponce on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Gaming_RLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
